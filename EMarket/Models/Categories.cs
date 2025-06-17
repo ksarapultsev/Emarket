dd5f3a26a105afc90a71d37c760f7d1dbf0f723a
ksapapultsev_laptop <ksarapultsev2014@gmail.com>
2025-06-17 11:16:23 +0600

    Add Categories Goods Sales models

---
+++ EMarket/Models/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMarket.Models
+{
+    public class Categories
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? RootId { get; set; }
+
+        public required string Name{ get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+    }
+}
