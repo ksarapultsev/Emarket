3c2ab964e94459458331c930f5211c25922ad19c
ksapapultsev_laptop <ksarapultsev2014@gmail.com>
2025-06-17 11:24:27 +0600

    Add migration DbContext

--- EMarket/Data/ApplicationDbContext.cs
+++ EMarket/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EMarket.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EMarket.Data;
@@ -8,5 +9,31 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<Categories> Categories => Set<Categories>();
+
+    public DbSet<Goods> Goods => Set<Goods>();  
+
+    public DbSet<Sales> Sales => Set<Sales>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder
+            .Entity<Categories>()
+            .Property(e => e.Created)
+            .HasDefaultValueSql("now()");
+
+        builder
+           .Entity<Goods>()
+           .Property(e => e.Created)
+           .HasDefaultValueSql("now()");
+
+        builder
+           .Entity<Sales>()
+           .Property(e => e.Created)
+           .HasDefaultValueSql("now()");
     }
 }
